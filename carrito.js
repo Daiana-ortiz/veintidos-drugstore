f19510f18b3d3e9299e68eae4a44b7ad71b9955a
Daiana Ortiz <daianayortiz@live.com>
2021-12-28 00:52:04 -0300

    falta que funcione el filter

--- carrito.js
+++ carrito.js
@@ -5,49 +5,57 @@
             id: 1,
             nombre: 'Turron',
             precio: 30,
-            imagen: 'images/turron.jpg'
+            imagen: 'images/turron.jpg',
+            categoria: "golosina"
         },
         {
             id: 2,
             nombre: 'Cofler Block',
             precio: 200,
-            imagen: 'images/block.jpg'
+            imagen: 'images/block.jpg',
+            categoria: "chocolate"
         },
         {
             id: 3,
             nombre: 'Rollo Mogul',
             precio: 45,
-            imagen: 'images/mogul.jpg'
+            imagen: 'images/mogul.jpg',
+            categoria: "golosina"
         },
         {
             id: 4,
             nombre: 'Pipas gigantes',
             precio: 80,
-            imagen: 'images/pipas.jpg'
+            imagen: 'images/pipas.jpg',
+            categoria: "snacks"
         },
         {
             id: 5,
             nombre: 'Alfajor Aguila',
             precio: 120,
-            imagen: 'images/aguila.jpg'
+            imagen: 'images/aguila.jpg',
+            categoria: "chocolate"
         },
         {
             id: 6,
             nombre: 'Paraguitas',
             precio: 65,
-            imagen: 'images/paraguitas.jpg'
+            imagen: 'images/paraguitas.jpg',
+            categoria: "chocolate"
         },
         {
             id: 7,
             nombre: 'Huevo Kinder',
             precio: 220,
-            imagen: 'images/huevo.jpg'
+            imagen: 'images/huevo.jpg',
+            categoria: "chocolate"
         },
         {
             id: 8,
             nombre: 'Kinder Bueno',
             precio: 250,
-            imagen: 'images/kinder.jpg'
+            imagen: 'images/kinder.jpg',
+            categoria: "chocolate"
         }
 
     ];
@@ -233,6 +241,46 @@
     // Eventos
     DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
+
+    
+
+//Funciones del filtro -> Recibe la categoria, hace el filtro y lo imprime en la web
+const filterButton = document.querySelectorAll('#filter-btn');
+
+
+const filterCategory = (categoria) => {
+	let categoryArray = baseDeDatos.filter(
+		(valor) => valor.categoria === categoria
+	);
+	renderizarProductos(categoryArray);
+};
+
+// funcion para filtrar el listado de productos
+
+function filterProducts(e) {
+	filterButton.forEach((button) => {
+		button.addEventListener('click', (e) => {
+			let buttonAlt = e.path[0].title;
+			console.log(buttonAlt);
+
+			if (buttonAlt === 'Todos') {
+				renderizarProductos(baseDeDatos);
+			}
+
+			if (buttonAlt === 'Chocolates') {
+				filterCategory('chocolate');
+			}
+
+			if (buttonAlt === 'Golosinas') {
+				filterCategory('golosina');
+			}
+
+			if (buttonAlt === 'Snacks') {
+				filterCategory('snacks');
+			}
+		});
+	});
+}
 
     // Inicio
     cargarCarritoDeLocalStorage();
