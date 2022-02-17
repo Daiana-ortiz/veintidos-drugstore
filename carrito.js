98298550383447eaf67f39ff08a6318f7b5a68c6
Daiana-ortiz <67471467+Daiana-ortiz@users.noreply.github.com>
2022-02-17 18:34:25 -0300

    styles

--- carrito.js
+++ carrito.js
@@ -1,66 +1,4 @@
-
-    // Variables
-    export const baseDeDatos = [
-        {
-            id: 1,
-            nombre: 'Turron',
-            precio: 30,
-            imagen: 'images/turron.jpg',
-            categoria: "Golosinas"
-        },
-        {
-            id: 2,
-            nombre: 'Cofler Block',
-            precio: 200,
-            imagen: 'images/block.jpg',
-            categoria: "Chocolates"
-        },
-        {
-            id: 3,
-            nombre: 'Rollo Mogul',
-            precio: 45,
-            imagen: 'images/mogul.jpg',
-            categoria: "Golosinas"
-        },
-        {
-            id: 4,
-            nombre: 'Pipas gigantes',
-            precio: 80,
-            imagen: 'images/pipas.jpg',
-            categoria: "Snacks"
-        },
-        {
-            id: 5,
-            nombre: 'Alfajor Aguila',
-            precio: 120,
-            imagen: 'images/aguila.jpg',
-            categoria: "Chocolates"
-        },
-        {
-            id: 6,
-            nombre: 'Paraguitas',
-            precio: 65,
-            imagen: 'images/paraguitas.jpg',
-            categoria: "Chocolates"
-        },
-        {
-            id: 7,
-            nombre: 'Huevo Kinder',
-            precio: 220,
-            imagen: 'images/huevo.jpg',
-            categoria: "Chocolates"
-        },
-        {
-            id: 8,
-            nombre: 'Kinder Bueno',
-            precio: 250,
-            imagen: 'images/kinder.jpg',
-            categoria: "Chocolates"
-        }
-
-    ];
-
-    
+import { baseDeDatos } from "./productos.js";
 
     let carrito = [];
     let total = 0;
@@ -330,6 +268,5 @@ const categorias = () => {
         
     }
 
-export default baseDeDatos
 
 
---
+++ productos.js
@@ -0,0 +1,61 @@
+export const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Turron',
+        precio: 30,
+        imagen: 'images/turron.jpg',
+        categoria: "Golosinas"
+    },
+    {
+        id: 2,
+        nombre: 'Cofler Block',
+        precio: 200,
+        imagen: 'images/block.jpg',
+        categoria: "Chocolates"
+    },
+    {
+        id: 3,
+        nombre: 'Rollo Mogul',
+        precio: 45,
+        imagen: 'images/mogul.jpg',
+        categoria: "Golosinas"
+    },
+    {
+        id: 4,
+        nombre: 'Pipas gigantes',
+        precio: 80,
+        imagen: 'images/pipas.jpg',
+        categoria: "Snacks"
+    },
+    {
+        id: 5,
+        nombre: 'Alfajor Aguila',
+        precio: 120,
+        imagen: 'images/aguila.jpg',
+        categoria: "Chocolates"
+    },
+    {
+        id: 6,
+        nombre: 'Paraguitas',
+        precio: 65,
+        imagen: 'images/paraguitas.jpg',
+        categoria: "Chocolates"
+    },
+    {
+        id: 7,
+        nombre: 'Huevo Kinder',
+        precio: 220,
+        imagen: 'images/huevo.jpg',
+        categoria: "Chocolates"
+    },
+    {
+        id: 8,
+        nombre: 'Kinder Bueno',
+        precio: 250,
+        imagen: 'images/kinder.jpg',
+        categoria: "Chocolates"
+    }
+
+];
+
+export default baseDeDatos
